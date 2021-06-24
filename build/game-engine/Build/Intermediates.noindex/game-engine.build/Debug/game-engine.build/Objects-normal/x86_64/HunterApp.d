760127ea1a4b41c1b1454c0e322e62bea06457ea
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-06-23 22:08:50 -0400

    Removed build folder

--- build/game-engine/Build/Intermediates.noindex/game-engine.build/Debug/game-engine.build/Objects-normal/x86_64/HunterApp.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/andymina/Coding/game-engine/game-engine/src/HunterApp.cpp \
-  /Users/andymina/Coding/game-engine/game-engine/include/HunterApp.h \
-  /Users/andymina/Coding/game-engine/game-engine/include/HunterCentral.h
